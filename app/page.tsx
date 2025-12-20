bbf21db7644e57e37364791f9e99d9a8892ada12
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-12-21 01:01:47 +0530

    update metadata titles and alt text in page.tsx to include a period for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,26 @@ import Home from "@/components/home";
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
-  title: "i should have told you",
+  title: "i should have told you.",
   description: "a personal archive",
   icons: {
     icon: "/logo.png",
   },
   openGraph: {
-    title: "i should have told you",
+    title: "i should have told you.",
     description: "a personal archive",
     images: [
       {
         url: "/og.png",
         width: 1200,
         height: 630,
-        alt: "i should have told you",
+        alt: "i should have told you.",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "i should have told you",
+    title: "i should have told you.",
     description: "a personal archive",
     images: ["/og.png"],
   },
